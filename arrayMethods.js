218e7c74bde629c3b7c7911eb7c40d222b63adad
Mattdre <olsen_matthew@yahoo.com>
2016-06-08 17:03:23 -1000

    finished up to concat

--- arrayMethods.js
+++ arrayMethods.js
@@ -33,5 +33,12 @@ mixedNums.sort();
 mixedWords.sort();
 
 var notFruit = fruitCollection.splice(1,3);
-gemBox.splice(5,2);
-gemBox.splice(8,4);
+gemBox.splice(5,7,'Sapphire');
+
+upToTen.splice(2,0,3,4,5,6,7,8);
+
+brownOnly.splice(1,5,'brown','brown','brown');
+
+var orderedValuesNewLength = orderedValues.unshift(1,2,3);
+
+var randomThingsArray = genericNumberArray.concat(colors);
