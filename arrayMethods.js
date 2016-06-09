0a6630542ac239c9290472b10885f38b6d1b8aef
Mattdre <olsen_matthew@yahoo.com>
2016-06-08 18:22:31 -1000

    finished up array methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -53,3 +53,11 @@ var favoriteFriends = friends.slice(1,3);
 var owesMoney = friends.slice(3,6);
 
 var monthNameString = monthNames.toString();
+
+var favoriteColor = colors.indexOf('Red');
+
+var favoriteEvenNumber = mixedNums.indexOf(76);
+
+var lastNine = bulkNumbers.lastIndexOf(9);
+
+var lastFive = bulkNumbers.lastIndexOf(5);
